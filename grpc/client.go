4b08b329115a374efa86dc45bf7e6e041cddc2db
4ubak <tima.75.com@gmail.com>
2026-01-15 18:07:40 +0500

    Fix: add missing metadata import in client.go

--- grpc/client.go
+++ grpc/client.go
@@ -10,6 +10,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/status"
 )
 
